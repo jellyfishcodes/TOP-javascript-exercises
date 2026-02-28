9151a77ff61d1a65b6e6c0f9b0d8f16fed8f1f43
jellyfish <76762587+jellyfishcodes@users.noreply.github.com>
2026-02-28 18:58:53 +0530

    Solved test 03.

--- foundations/03_numberChecker/numberChecker.js
+++ foundations/03_numberChecker/numberChecker.js
@@ -1,7 +1,13 @@
 function numberChecker(number) {
-  if (number === 6) {
+  if (number === 1000 || number === 10) {
     return true;
-  } else {
+  } 
+
+  else if(number === 6 || number === 9){
+    return false;
+  }
+  
+  else {
     return false;
   }
 }
