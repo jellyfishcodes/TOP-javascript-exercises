c1d827ba3512e1ff78c22836d5c17c88d335e632
jellyfish <76762587+jellyfishcodes@users.noreply.github.com>
2026-03-17 02:04:09 +0530

    Solved test number 6, YAYgit add .git add .git add .!

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,33 @@
-const repeatString = function() {
+const repeatString = function(str,num) {
+    let str1 = '';
 
+    for(i = 0; i< num; i++){
+
+        if(num == 1){str1 = str; break;}
+
+        str1 += str;
+    }
+
+    if(num < 0){str1 = 'ERROR'};
+
+    return str1;
 };
+
+repeatString('hey',3);
+
+repeatString('hello',10);
+
+repeatString('hi', 1);
+
+repeatString('bye', 0);
+
+repeatString('hey', 2);
+
+repeatString("don't use the built-in repeat method!", 1);
+
+repeatString('goodbye', -1);
+
+repeatString('', 10);
 
 // Do not edit below this line
 module.exports = repeatString;
