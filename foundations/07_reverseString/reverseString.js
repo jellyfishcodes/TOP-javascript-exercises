580c955247812e36086ec290952f5a42264f3f77
jellyfish <76762587+jellyfishcodes@users.noreply.github.com>
2026-03-17 03:45:17 +0530

    Solved test number 7

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,21 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let str1 = '';
 
+    for(let i = (str.length)-1; i >= 0; i--){
+
+        str1 += str[i];
+    }
+
+    return str1;
 };
+
+/*
+    can also write it like this,
+
+    const reverseString = function(str) {
+    return str.split('').reverse().join('');
+    }
+*/
 
 // Do not edit below this line
 module.exports = reverseString;
