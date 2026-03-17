37a0ed2cd76b3fb083d2b79b1e73d7ac6b791a7b
jellyfish <76762587+jellyfishcodes@users.noreply.github.com>
2026-03-17 05:45:15 +0530

    Solved test number 8

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, n, m, o, p) {
+    return arr.filter(item => (item !== n) && (item !== m) && (item !== o) && (item !== p));
 };
 
 // Do not edit below this line
