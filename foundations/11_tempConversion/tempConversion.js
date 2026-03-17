4cfb0ef2344f474e027b69eadb79e089f97de388
jellyfish <76762587+jellyfishcodes@users.noreply.github.com>
2026-03-18 04:22:20 +0530

    Solved test number 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let celsius = (num - 32)  * (5/9);
+
+  let roundedCelsius = celsius.toFixed(1);
+  let finalCelsius = +roundedCelsius;
+
+  return finalCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num1) {
+  let fahrenheit = (num1 * 9/5) + 32;
+
+  let roundedFahrenheit = fahrenheit.toFixed(1);
+  let finalFahrenheit = +roundedFahrenheit;
+
+  return finalFahrenheit;
 };
 
 // Do not edit below this line
